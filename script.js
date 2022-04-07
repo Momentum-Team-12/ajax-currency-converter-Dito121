8ccd94cb6b2ef73319d839a9d1227d09e3efab86
Gerardo Ayes <gerardoayes92@gmail.com>
2022-04-06 20:47:06 -0400

    stuck on getting user input data and multiplying by correct conversion factor then displaying result

--- script.js
+++ script.js
@@ -1,34 +1,6 @@
+let initialDiv = document.getElementById('initial')
+let finalDiv = document.getElementById('final')
 let contentDiv = document.getElementById('content')
-
-fetch('https://openexchangerates.org/api/latest.json?app_id=8aa1c434ba634b4f938302271614ce30', {
-    method: 'GET',
-    headers: {},
-})
-.then(function(response) {
-    return response.json()
-})
-.then(function(data) {
-    console.log(data.rates)
-
-    let values = ["dog", "cat", "parrot", "rabbit"];
-
-    let select = document.createElement("select");
-    select.name = "pets";
-    select.id = "pets"
-
-    for (let val of values) {
-        let option = document.createElement("option");
-        option.value = val;
-        option.text = val.charAt(0).toUpperCase() + val.slice(1);
-        select.appendChild(option);
-    }
-
-    let label = document.createElement("label");
-    label.innerHTML = "Choose your pets: "
-    label.htmlFor = "pets";
-
-    document.getElementById("content").appendChild(label).appendChild(select);
-})
 
 fetch("https://openexchangerates.org/api/currencies.json", {
     method: 'GET',
@@ -39,24 +11,76 @@ fetch("https://openexchangerates.org/api/currencies.json", {
 })
 .then(function(data) {
     console.log(data)
+    return data
 })
 
 
-// let values = ["dog", "cat", "parrot", "rabbit"];
+fetch('https://openexchangerates.org/api/latest.json?app_id=8aa1c434ba634b4f938302271614ce30', {
+    method: 'GET',
+    headers: {},
+})
+.then(function(response) {
+    return response.json()
+})
+.then(function(data) {
+    let conversions = data.rates;
+    console.log(conversions)
+    let keys = Object.keys(conversions)
 
-// let select = document.createElement("select");
-// select.name = "pets";
-// select.id = "pets"
+    let selectInitial = document.createElement("select");
+    selectInitial.name = "initialCurrency";
+    selectInitial.id = "initialCurrency"
 
-// for (let val of values) {
-//     let option = document.createElement("option");
-//     option.value = val;
-//     option.text = val.charAt(0).toUpperCase() + val.slice(1);
-//     select.appendChild(option);
-// }
+    for (let key of keys) {
+        let option = document.createElement("option");
+        option.id = key;
+        option.innerText = key;
+        selectInitial.appendChild(option);
+    }
 
-// let label = document.createElement("label");
-// label.innerHTML = "Choose your pets: "
-// label.htmlFor = "pets";
+    let label = document.createElement("label");
+    let innerDiv = document.createElement("div");
+    innerDiv.innerText = "Choose initial currency: "
+    label.appendChild(innerDiv);
 
-// document.getElementById("content").appendChild(label).appendChild(select);
+    initialDiv.appendChild(label).appendChild(selectInitial);
+
+    function formData() {
+        let amountBefore = document.getElementById("amount").value;
+
+        let conv = document.getElementById('final')
+        let conversion = conv.value
+        console.log(conversion)
+
+        console.log(conversions.conversion)
+        let answer = amountBefore * conversions.conversion
+
+        document.writeln("<h1>Confirmation Page</h1><br>");
+    }
+
+    return data;
+})
+.then(function(data) {
+
+    let values = data.rates;
+    let keys = Object.keys(data.rates)
+
+    let selectFinal = document.createElement("select");
+    selectFinal.name = "finalCurrency";
+    selectFinal.id = "finalCurrency"
+
+    for (let key of keys) {
+        let option = document.createElement("option");
+        option.value = key;
+        option.innerText = key;
+        selectFinal.appendChild(option);
+    }
+
+    let label = document.createElement("label");
+    let innerDiv = document.createElement("div");
+    innerDiv.innerText = "Choose final currency: "
+    label.appendChild(innerDiv);
+
+    finalDiv.appendChild(label).appendChild(selectFinal);
+})
+
